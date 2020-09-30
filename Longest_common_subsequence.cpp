6d1778239e5dfa31c62a4cdb52159309e6ddca94
Firoz kumar <56572016+Firoz-kumar-3@users.noreply.github.com>
2020-09-30 13:21:12 +0530

    Update Longest_common_subsequence.cpp

--- Longest_common_subsequence.cpp
+++ Longest_common_subsequence.cpp
@@ -34,3 +34,64 @@ int main() {
 	cout<<lcs(s,s2);
 	return 0;
 }
+
+
+//recursive solution
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int lcs(string s1,string s2,int i,int j,int dp[101][101])
+{
+    if(i==s1.length()|| j==s2.length())
+    {
+        return 0;
+    }
+  if(dp[i][j]!=-1)
+   {
+       return dp[i][j];
+   }
+  if(s1[i]==s2[j])
+  {
+      dp[i][j]=1 + lcs(s1,s2,i+1,j+1,dp);
+  }
+  else 
+  {
+     dp[i][j]=max(lcs(s1,s2,i,j+1,dp),lcs(s1,s2,i+1,j,dp));
+  }
+  return dp[i][j];
+}
+
+
+
+int main() {
+	//code
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n1,n2;
+	    cin>>n1>>n2;
+	    cin.get();
+	    string s1,s2;
+	    cin>>s1>>s2;
+	    int dp[101][101];
+	    for(int i=0;i<n1;i++)
+	    {
+	        for(int j=0;j<n2;j++)
+	        {
+	            dp[i][j]=-1;
+	        }
+	    }
+	   int l=lcs(s1,s2,0,0,dp);
+	   cout<<l<<endl;
+	    
+	}
+	
+	
+	
+	
+	return 0;
+}
